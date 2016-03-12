97eb335f6eb7a1d2e23ee54d7cc6e61408d7001d
Carlo Alberto Negri <calber@calber.org>
2016-03-12 15:22:00 +0100

    WIP

--- app/src/main/java/org/calber/hue/MainActivity.java
+++ app/src/main/java/org/calber/hue/MainActivity.java
@@ -52,8 +52,10 @@ public class MainActivity extends AppCompatActivity implements FragmentInteracti
 
         navigator = new Navigator(getSupportFragmentManager(), R.id.root);
 
-//        getSharedPreferences("Hue", Context.MODE_PRIVATE).edit().putString("URL", "http://10.0.0.12/").commit();
-//        getSharedPreferences("Hue", Context.MODE_PRIVATE).edit().putString("TOKEN", "dee4feb2224a44f254acf32ed98dd7").commit();
+        if(BuildConfig.BUILD_TYPE.equals("nonetwork")) {
+            getSharedPreferences("Hue", Context.MODE_PRIVATE).edit().putString("URL", "http://10.0.0.12/").commit();
+            getSharedPreferences("Hue", Context.MODE_PRIVATE).edit().putString("TOKEN", "dee4feb2224a44f254acf32ed98dd7").commit();
+        }
 
         Hue.TOKEN = getSharedPreferences("Hue", Context.MODE_PRIVATE).getString("TOKEN", null);
         Hue.URL = getSharedPreferences("Hue", Context.MODE_PRIVATE).getString("URL", null);
